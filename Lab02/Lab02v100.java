42cf16fb41307180c40e250714bc8ddd49c05c76
Duncan Antich <duncan.a19@student.parishepiscopal.org>
2018-09-12 14:45:41 -0500

    Added

---
+++ Lab02/Lab02v100.java
@@ -0,0 +1,24 @@
+//Lab02v100.java
+//The 100-point version of Lab02 is an exercise in precisely copying a
+//provided program, followed by compiling the source code into bytecode
+//and executing the bytecode file.
+
+public class Lab02v100
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Lab 02, 100 Point Version");
+        System.out.println();
+        System.out.println();
+        System.out.print("Measure yesterday's knowledge ");
+        System.out.println("by yesterday's confusion.");
+        System.out.println();
+        System.out.println("Bewilderment + Exposure = Obvious");
+        System.out.println();
+        System.out.println("# ##### #####    ##### ##### # #   # ##### ######");
+        System.out.println("# #   # #   #    #   # #   # # ##  #   #   #");
+        System.out.println("# #   # #   #    ##### #   # # # # #   #   ######");
+        System.out.println("# #   # #   #    #     #   # # #  ##   #        #");
+        System.out.println("# ##### #####    #     ##### # #   #   #   ######");
+    }
+}    
